12da64cd4d02b287c463e230f8e4b8fbc0baa892
huangcong <mail.huangcong@gmail.com>
2015-01-30 02:54:46 +0800

    animator.test.release

---
+++ app/src/main/java/com/huangcong/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.huangcong.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button)this.findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.animator.animation1, R.animator.animation2);
+            }
+        });
+    }
+}
